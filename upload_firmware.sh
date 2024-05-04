e4634e21f5fa8675b107528ffae2b2a7b94c5562
Andrei Dolgov <andrei@dolgov.dev>
2024-05-04 15:50:36 -0400

    feat: script to upload firmware to keyboard

--- upload_firmware.sh
+++ upload_firmware.sh
@@ -5,10 +5,61 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-while [ ! -f /dev/sda ] ; do sleep 1; printf 'Waiting for the right side to be connected...\r'; done
+device=""
+firmware=''
 
-printf "Right side has been updated\n"
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --device=*)
+            device="${1#*=}"
+            ;;
+        --firmware=*)
+           firmware="${1#*=}"
+            ;;
+        *)
+            echo "Unknown parameter passed: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
 
-while [ ! -f /dev/sda ] ; do sleep 1; printf 'Waiting for the left side to be connected...\r'; done
+# Check if required arguments are provided
+if [ -z "$device" ] || [ -z "$firmware" ]; then
+    echo "Usage: ./upload_firmware.sh --device=/dev/sda --firmware=~/Downloads/glove80.uf2"
+    exit 1
+fi
+
+
+mkdir -p /tmp/glove80
+
+while [ ! -e "$device" ]; do
+    sleep 1
+    printf 'Waiting for the right side to be connected...\r'
+done
+
+mount $device /tmp/glove80
+cp $firmware /tmp/glove80
+umount /tmp/glove80
+
+printf "Right side has been updated\n\n"
+
+for (( i=10; i>=1; i-- )); do
+    echo -ne " $i second(s) \r"
+    sleep 1
+done
+
+while [ ! -e "$device" ]; do
+    sleep 1
+    printf 'Waiting for the left side to be connected...\r'
+done
+
+mount $device /tmp/glove80
+cp $firmware /tmp/glove80
+umount /tmp/glove80
+
+printf "Left side has been updated\n\n"
+
+sleep 1
 
 printf "Done! Restart the keyboard\n"
