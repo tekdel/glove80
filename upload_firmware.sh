5d90efc1adae490e251937660267f8cdee582dbd
Andrei Dolgov <andrei@dolgov.dev>
2024-05-04 15:16:25 -0400

    feat: dvorak

--- upload_firmware.sh
+++ upload_firmware.sh
@@ -1,5 +1,10 @@
 #!/bin/env bash
 
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
 while [ ! -f /dev/sda ] ; do sleep 1; printf 'Waiting for the right side to be connected...\r'; done
 
 printf "Right side has been updated\n"
